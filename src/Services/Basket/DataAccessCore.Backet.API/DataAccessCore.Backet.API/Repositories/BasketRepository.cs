8d00d07d78b5a876321ae2cc049914eff01e70f9
Andrey Medvedev <andreygasparyan539@gmail.com>
2021-08-12 18:39:31 +0300

    Added repository

---
+++ src/Services/Basket/DataAccessCore.Backet.API/DataAccessCore.Backet.API/Repositories/BasketRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessCore.Backet.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+
+namespace DataAccessCore.Backet.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public Task DeleteBasketAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ShoppingCart> GetBasketAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ShoppingCart> UpdateBasketAsync(ShoppingCart shoppingCart)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
