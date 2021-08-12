4589cc19dc2b309a56471aa7855056ad68b4f574
Andrey Medvedev <andreygasparyan539@gmail.com>
2021-08-12 18:29:09 +0300

    Update project

---
+++ src/Services/Basket/DataAccessCore.Backet.API/DataAccessCore.Backet.API/Entities/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessCore.Backet.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItem { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (ShoppingCartItem item in ShoppingCartItem)
+                {
+                    total += item.Price * item.Quantity;
+                }
+
+                return total;
+            }
+        }
+
+    }
+}
---
+++ src/Services/Basket/DataAccessCore.Backet.API/DataAccessCore.Backet.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccessCore.Backet.API.Repositories
+{
+    using DataAccessCore.Backet.API.Entities;
+    using System.Threading.Tasks;
+
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketAsync(string userName);
+
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart shoppingCart);
+
+        Task DeleteBasketAsync(string userName);
+    }
+}
