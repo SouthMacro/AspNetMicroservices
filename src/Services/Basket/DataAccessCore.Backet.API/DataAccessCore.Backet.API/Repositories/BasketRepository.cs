bea82ac16bd13948aba39633aff19ea0fcc7f3ea
Andrey Medvedev <andreygasparyan539@gmail.com>
2021-08-12 19:14:52 +0300

    Update method to getbacket

--- src/Services/Basket/DataAccessCore.Backet.API/DataAccessCore.Backet.API/Repositories/BasketRepository.cs
+++ src/Services/Basket/DataAccessCore.Backet.API/DataAccessCore.Backet.API/Repositories/BasketRepository.cs
@@ -1,24 +1,37 @@
-﻿using DataAccessCore.Backet.API.Entities;
-using Microsoft.Extensions.Caching.Distributed;
-using System.Threading.Tasks;
-
-namespace DataAccessCore.Backet.API.Repositories
+﻿namespace DataAccessCore.Backet.API.Repositories
 {
+    using DataAccessCore.Backet.API.Entities;
+    using Microsoft.Extensions.Caching.Distributed;
+    using Newtonsoft.Json;
+    using System;
+    using System.Threading.Tasks;
+
     public class BasketRepository : IBasketRepository
     {
         private readonly IDistributedCache _distributedCache;
 
-        public Task DeleteBasketAsync(string userName)
+        public BasketRepository(IDistributedCache distributedCache)
         {
-            throw new System.NotImplementedException();
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
         }
-
-        public Task<ShoppingCart> GetBasketAsync(string userName)
+        public async Task<ShoppingCart> GetBasketAsync(string userName)
         {
-            throw new System.NotImplementedException();
+            var backet = await _distributedCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(backet))
+            {
+                throw new ArgumentException(string.Empty, nameof(backet));
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(backet);
         }
 
         public Task<ShoppingCart> UpdateBasketAsync(ShoppingCart shoppingCart)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteBasketAsync(string userName)
         {
             throw new System.NotImplementedException();
         }
